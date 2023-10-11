9d1770fe384fe7bd5995988efbffdad6c81c8fd0
minhanPark <alsgks1218@naver.com>
2023-10-11 17:52:22 +0900

    cron으로 사용할 route 추가

---
+++ app/api/cron/route.ts
@@ -0,0 +1,76 @@
+import Product from "@/lib/models/product.model";
+import { connectToDB } from "@/lib/mongoose";
+import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
+import { scrapeAmazonProduct } from "@/lib/scraper";
+import {
+  getAveragePrice,
+  getEmailNotifType,
+  getHighestPrice,
+  getLowestPrice,
+} from "@/lib/utils";
+import { Product as ProductType, User } from "@/types";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    connectToDB();
+
+    const products = await Product.find({});
+    if (!products) throw new Error("No products found");
+
+    const updatedProducts = await Promise.all(
+      products.map(async (currentProduct: ProductType) => {
+        const scrapedProduct = await scrapeAmazonProduct(currentProduct.url);
+
+        if (!scrapedProduct) return;
+
+        const updatedPriceHistory = [
+          ...currentProduct.priceHistory,
+          {
+            price: scrapedProduct.currentPrice,
+          },
+        ];
+
+        const product = {
+          ...scrapedProduct,
+          priceHistory: updatedPriceHistory,
+          lowestPrice: getLowestPrice(updatedPriceHistory),
+          highestPrice: getHighestPrice(updatedPriceHistory),
+          averagePrice: getAveragePrice(updatedPriceHistory),
+        };
+        const updatedProduct = await Product.findOneAndUpdate(
+          {
+            url: product.url,
+          },
+          product
+        );
+
+        const emailNotifType = getEmailNotifType(
+          scrapedProduct,
+          currentProduct
+        );
+
+        if (emailNotifType && updatedProduct.users.length > 0) {
+          const productInfo = {
+            title: updatedProduct.title,
+            url: updatedProduct.url,
+          };
+          const emailContent = await generateEmailBody(
+            productInfo,
+            emailNotifType
+          );
+
+          const usersEmails = updatedProduct.users.map(
+            (user: User) => user.email
+          );
+
+          await sendEmail(emailContent, usersEmails);
+        }
+        return updatedProduct;
+      })
+    );
+    return NextResponse.json({ message: "ok", data: updatedProducts });
+  } catch (error) {
+    throw new Error(`Error in GET: ${error}`);
+  }
+}
--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -65,7 +65,7 @@ export async function scrapeAmazonProduct(url: string) {
       discountRate: Number(discountRate),
       category: "category",
       reviewsCount: 100,
-      starts: 4.5,
+      stars: 4.5,
       isOutOfStock: outOfStock,
       description,
       lowestPrice: Number(currentPrice) || Number(originalPrice),
